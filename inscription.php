7ef622efcb1adddd81fd700b863ad018d41c68a3
Adil Benameur <adil.benameur1@gmail.com>
2019-01-01 17:23:50 +0100

    Modification des identifiants utilisateurs (auto-incrémentés) Finalisation partie php de la page inscription

--- inscription.php
+++ inscription.php
@@ -1,7 +1,28 @@
 <?php
     include("mysql.php");
-    $mysqli->real_query("SELECT id_eleve, nom, prenom, qui FROM eleves");
-    $res = $mysqli->use_result();
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $post = True;
+        $email = htmlspecialchars($_POST["email"]);
+        $row = mysqli_fetch_array(mysqli_query($mysqli, "SELECT email FROM eleves WHERE email = \"" . $email . "\";"));
+        if ($row["email"] == $email) {
+            $email_exit = True;
+        }
+        else {
+            $password = htmlspecialchars($_POST["password"]);
+            $prenom = htmlspecialchars($_POST["prenom"]);
+            $qui = htmlspecialchars($_POST["qui"]);
+            $parcours = htmlspecialchars($_POST["parcours"]);
+            $nom = htmlspecialchars($_POST["nom"]);
+            mysqli_query($mysqli, "INSERT INTO eleves (nom, prenom, email, password, qui, parcours) VALUES(\"". $nom ."\",\"". $prenom ."\",\"". $email ."\",\"". $password ."\",\"". $qui ."\",\"". $parcours ."\");");
+            if (isset($_FILES['avatar'])) {
+                # Image profil
+                $row = mysqli_fetch_array(mysqli_query($mysqli,"SELECT id_eleve FROM eleves WHERE email = \"" . $email . "\";"));
+                error_log($row["id_eleve"], $message_type=0);
+                move_uploaded_file($_FILES['avatar']['tmp_name'], 'uploads/' . $row["id_eleve"] . pathinfo($_FILES['avatar']['name'])['extension']);
+            }
+        }
+        mysqli_close($mysqli);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +30,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Inscrivez-vous dzns le registre</title>
+    <title>Inscrivez-vous dans le registre</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/css/bootstrap.min.css">
 </head>
 
@@ -40,12 +61,12 @@
 
     <div class="container-fluid" style="margin-top:20px;">
         <div id="kv-avatar-errors-2" class="center-block" style="width:800px;display:none"></div>
-        <form class="form form-vertical" action="/avatar_upload.php" method="post" enctype="multipart/form-data">
+        <form class="form form-vertical" action="/inscription.php" method="post" enctype="multipart/form-data">
             <div class="row">
                 <div class="col-sm-4 text-center">
                     <div class="kv-avatar">
                         <div class="file-loading">
-                            <input id="avatar-2" name="avatar-2" type="file" required>
+                            <input id="avatar-2" name="avatar" type="file">
                         </div>
                     </div>
                 </div>
@@ -59,8 +80,8 @@
                         </div>
                         <div class="col-sm-6">
                             <div class="form-group">
-                                <label for="pwd">Mot de passe<span class="kv-reqd">*</span></label>
-                                <input type="password" class="form-control" name="pwd" required>
+                                <label for="password">Mot de passe<span class="kv-reqd">*</span></label>
+                                <input type="password" class="form-control" name="password" required>
                             </div>
                         </div>
                     </div>
@@ -90,7 +111,7 @@
                         <div class="col">
                             <div class="form-group">
                                 <label for="parcours">Mon parcours</label>
-                                <textarea class="form-control" rows="5" id="parcours"></textarea>
+                                <textarea class="form-control" rows="5" name="parcours"></textarea>
                             </div>
                         </div>
                     </div>
@@ -104,25 +125,25 @@
         </form>
     </div>
 
-    
+
     <script>
         var btnCust = '<button type="button" class="btn btn-secondary" title="Add picture tags" ' +
             'onclick="alert(\'Call your custom code here.\')">' +
             '<i class="glyphicon glyphicon-tag"></i>' +
             '</button>';
         $("#avatar-2").fileinput({
-            overwriteInitial: true,
+            overwriteInitial: True,
             maxFileSize: 1500,
             showClose: false,
             showCaption: false,
             showBrowse: false,
-            browseOnZoneClick: true,
+            browseOnZoneClick: True,
             removeLabel: '',
             removeIcon: '<i class="glyphicon glyphicon-remove"></i>',
             removeTitle: 'Cancel or reset changes',
             elErrorContainer: '#kv-avatar-errors-2',
             msgErrorClass: 'alert alert-block alert-danger',
-            defaultPreviewContent: '<img src="image/default_avatar_male.jpg" alt="Your Avatar"><h6 class="text-muted">Click to select</h6>',
+            defaultPreviewContent: '<img src="images/default_profil.jpg" alt="Votre avatar"><h6 class="text-muted">Click to select</h6>',
             layoutTemplates: {
                 main2: '{preview} ' + btnCust + ' {remove} {browse}'
             },
@@ -136,5 +157,4 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
         crossorigin="anonymous"></script>
 </body>
-
 </html>
