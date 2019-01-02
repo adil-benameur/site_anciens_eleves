f5a75a8a6f6e859e50964846d8c215299a2e3f10
Adil Benameur <adil.benameur1@gmail.com>
2019-01-02 04:08:02 +0100

    no message

---
+++ mysql.php
@@ -0,0 +1,5 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    $mysqli = mysqli_connect("bh0xvwxrn-mysql.services.clever-cloud.com", "uhsbsls3esxuhstw", "rvFILyN1eBDmvUXHZKj", "bh0xvwxrn");
+?>
