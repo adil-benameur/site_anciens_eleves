dd7ed73c6e115fa5ccd9f21290f24ec65dd66b4c
Adil Benameur <adil.benameur1@gmail.com>
2019-01-13 19:58:47 +0100

    Update 3.php

--- isn/3.php
+++ isn/3.php
@@ -1,42 +1,29 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
 </head>
+
 <body>
-    <thead>
-        <tr>
-            <th>Envoie et récupération de données</th>
-        </tr>
-    </thead>
+    <h2>Envoie et récupération de données</h2>
     <form action="/isn/3.php" method="post" enctype="multipart/form-data">
-        <tbody>
-            <tr>
-                <td>
-                    <label for="nom">Nom</label>
-                    <input name="nom">
-                    <br />
-                </td>
-                <td>
-                    <h3>Nom</h3>
-                    <p> <?php print($_POST["nom"]); ?> </p>
-                    <br />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="prenom">Prénom</label>
-                    <input name="prenom">
-                    <br />
-                </td>
-                <td>
-                    <h3>Prénom</h3>
-                    <p> <?php print($_POST["prenom"]); ?> </p>
-                    <br />
-                </td>
-            </tr>
-        </tbody>
+        <label for="nom">Entrez un nom :</label>
+        <input name="nom">
+        <br />
+        <h3>Nom :
+            <?php print($_POST["nom"]); ?>
+        </h3>
+        <br />
+        <label for="prenom">Entrez un prénom :</label>
+        <input name="prenom">
+        <br />
+        <h3>Prénom :
+            <?php print($_POST["prenom"]); ?>
+        </h3>
+        <br />
         <button type="submit">Envoyer</button>
     </form>
 </body>
+
 </html>
